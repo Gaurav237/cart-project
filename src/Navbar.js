313c033079eb8bb9b186268b21411f87a846f16b
Gaurav237 <gktiwary237@gmail.com>
2023-06-20 07:46:40 +0530

    added Navbar Component

---
+++ src/Navbar.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Navbar = () => {
+
+    return (
+        <div style={styles.nav}>
+            <div style={styles.header}>
+                <h1>CART</h1>
+            </div>
+            <div style={styles.cartIconContainer} >
+                <img style={styles.cartIcon} alt="cart-icon" src="https://cdn-icons-png.flaticon.com/512/3144/3144456.png" />
+                <span style={styles.cartCount} >3</span>
+            </div>
+        </div>
+    );
+}
+
+const styles = {
+    header: {
+      marginLeft: 20,
+      color: 'white',
+      fontSize: 22
+    },
+    cartIcon: {
+      height: 32,
+      marginRight: 20
+    },
+    nav: {
+      height: 70,
+      background: '#4267b2',
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+    },
+    cartIconContainer: {
+      position: 'relative'
+    },
+    cartCount: {
+      background: 'yellow',
+      borderRadius: '50%',
+      padding: '4px 8px',
+      position: 'absolute',
+      right: 0,
+      top: -9
+    }
+  };
+  
+
+export default Navbar;
